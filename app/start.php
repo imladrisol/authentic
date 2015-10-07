afdc9e7aac7c0b982a9a12a76dcadb0ddc86fa8e
imladris <imladrisol@gmail.com>
2015-10-07 17:02:57 +0300

    Running Test application

--- app/start.php
+++ app/start.php
@@ -1,4 +1,6 @@
 <?php
+use Slim\Slim; // namespace/class
+
 /* Turn off server's headers Cache-Control, Pragma, Expires denied browsers cashing */
 session_cache_limiter(false);
 session_start();
@@ -8,3 +10,11 @@ ini_set('display_errors', 'On');
 
 define('INC_ROOT', dirname(__DIR__));
 require INC_ROOT."/vendor/autoload.php";
+
+$app = new Slim();
+/*
+ //Test sample
+$app->get('/test/:name', function($name){
+    echo "Hello {$name}";
+});
+*/
